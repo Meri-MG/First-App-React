d5d5e6074ccee43750b3aed2af66d0f4dcac8707
Meri <mgogi13@freeuni.edu.ge>
2021-12-18 21:20:10 +0400

    Naive approach to components

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,19 +2,57 @@
 
 import React from 'react';
 import reactDOM from 'react-dom';
+import faker from 'faker';
 
 // Create a react component
 const App = () => {
-	const style = {backgroundColor: 'blue', color: 'white'};
-    return (
-		  <div>
-				<label className="label" htmlFor="name">
-				Enter name: 
-				</label>
-				<input id="name" type="text" />
-				<button style={style}>Submit</button>
+	return (
+    <div className="ui container comments">
+		  <div className="comment">
+        <a href="/" className="avatar">
+					<img alt="avatar" src={ faker.image.image()}/>
+				</a>
+				<div className="content">
+				  <a href="/" className="author">
+					Meri
+					</a>
+					<div className="metadata">
+					  <span className="date"> Today at 6:00PM</span>
+					</div>
+					<div className="text"> Nice post blog!</div>
+				</div>
 			</div>
-		);
+			<div className="comment">
+        <a href="/" className="avatar">
+					<img alt="avatar" src={ faker.image.image()}/>
+				</a>
+				<div className="content">
+				  <a href="/" className="author">
+					Meri
+					</a>
+					<div className="metadata">
+					  <span className="date"> Today at 6:00PM</span>
+					</div>
+					<div className="text"> Nice post blog!</div>
+				</div>
+			</div>
+			<div className="comment">
+        <a href="/" className="avatar">
+					<img alt="avatar" src={ faker.image.image()}/>
+				</a>
+				<div className="content">
+				  <a href="/" className="author">
+					Meri
+					</a>
+					<div className="metadata">
+					  <span className="date"> Today at 6:00PM</span>
+					</div>
+					<div className="text"> Nice post blog!</div>
+				</div>
+			</div>
+		</div>
+				
+	);
 };
 
 // Take the react component and show it on the screen
