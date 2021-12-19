30b8261b5f69cbfca6745c1f33b8e4ea611dfae9
Meri <mgogi13@freeuni.edu.ge>
2021-12-19 05:51:44 +0400

    Make components reusable

--- jsx/src/CommentDetail.js
+++ jsx/src/CommentDetail.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = (props) => {
-    console.log(props)
     return (
       <div className="comment">
         <a href="/" className="avatar">
-					<img alt="avatar" src={ faker.image.image()}/>
+					<img alt="avatar" src={ props.avatarPic }/>
 				</a>
 				<div className="content">
 				  <a href="/" className="author">
 					{props.author}
 					</a>
 					<div className="metadata">
-					  <span className="date"> Today at 6:00PM</span>
+					  <span className="date">{ props.timeAgo }</span>
 					</div>
-					<div className="text"> Nice post blog!</div>
+					<div className="text">{ props.commentText }</div>
 				</div>
 			</div>
     );
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,14 +4,21 @@ import React from 'react';
 import reactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // Create a react component
 const App = () => {
 	return (
     <div className="ui container comments">
-		<CommentDetail author="Meri"/>
-		<CommentDetail author="Steven"/>
-		<CommentDetail author="Aku"/>
+		<ApprovalCard>
+		  <CommentDetail author="Meri" timeAgo="Today at 5:00PM" commentText="Nice blog post!" avatarPic={ faker.image.image() }/>
+		</ApprovalCard>
+		<ApprovalCard>
+		  <CommentDetail author="Steven" timeAgo="Today at 9:00AM" commentText="Nice blog post!" avatarPic={ faker.image.image() }/>
+		</ApprovalCard>
+		<ApprovalCard>
+		  <CommentDetail author="Aku" timeAgo="yesterday at 2:00PM" commentText="Nice blog post!" avatarPic={ faker.image.image() }/>
+		</ApprovalCard>
 		</div>
 				
 	);
